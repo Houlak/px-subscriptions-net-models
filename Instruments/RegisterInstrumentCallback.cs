88e800854ff34fdca0f81de93ee00699ce7aaa28
Lucas <lucas.suburuch@gmail.com>
2019-09-03 13:49:22 -0300

    Models updated for callback

---
+++ Instruments/RegisterInstrumentCallback.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Goova.Subscriptions.Models.Instruments
+{
+    public class RegisterInstrumentCallback
+    {
+        public bool RegisterSuccess { get; set; }
+        public string ExternalId { get; set; }
+    }
+}
