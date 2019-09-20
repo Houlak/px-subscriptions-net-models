de2cf5280457cdf0558823f51eaa507937d46f62
Lucas <lucas.suburuch@gmail.com>
2019-09-20 10:16:12 -0300

    master models generic

--- Clients/AddClientRequest.cs
+++ Clients/AddClientRequest.cs
@@ -9,7 +9,5 @@ namespace Goova.Subscriptions.Models.Clients
         public string AdminUserId { get; set; }
         public string ClientName { get; set; }
         public string ClientSecret { get; set; }
-        public string ClientLegalName { get; set; }
-        public string ClientRFC { get; set; }
     }
 }
