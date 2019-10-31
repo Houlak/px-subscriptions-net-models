e5e81022a3edf7e048a4a0bbd8def64d03272f31
Lucas <lucas.suburuch@gmail.com>
2019-10-31 15:43:08 -0300

    Added callbackResponse class

---
+++ Instruments/InstrumentCallbackResponse.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Goova.Subscriptions.Models.Instruments
+{
+    public class InstrumentCallbackResponse
+    {
+        public bool RegisterSuccess { get; set; }
+        public string ExternalId { get; set; }
+        public Instrument Instrument { get; set; }
+    }
+}
