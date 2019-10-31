62048dee1cb4e7c5efd05a5436f0d0af6f6d722e
Lucas <lucas.suburuch@gmail.com>
2019-10-31 11:37:35 -0300

    Added clientLegal aspects

--- Clients/AddClientRequest.cs
+++ Clients/AddClientRequest.cs
@@ -9,5 +9,7 @@ namespace Goova.Subscriptions.Models.Clients
         public string AdminUserId { get; set; }
         public string ClientName { get; set; }
         public string ClientSecret { get; set; }
+        public string ClientLegalName { get; set; }
+        public string ClientLegalId { get; set; }
     }
 }
