e7fd62092f52c70039a8fb452fe69a17c32dff24
Lucas <lucas.suburuch@gmail.com>
2019-11-14 12:50:45 -0300

    Added instrument to transaction

--- Transactions/Transaction.cs
+++ Transactions/Transaction.cs
@@ -1,4 +1,5 @@
-﻿using Goova.Subscriptions.Models.SubscriptionType;
+﻿using Goova.Subscriptions.Models.Instruments;
+using Goova.Subscriptions.Models.SubscriptionType;
 using System;
 
 namespace Goova.Subscriptions.Models.Transactions
@@ -13,7 +14,7 @@ namespace Goova.Subscriptions.Models.Transactions
         public DateTime CreatedAt { get; set; }
         public TransactionState State { get; set; }
         public Currency Currency { get; set; }
-        //public Instrument Instrument { get; set; } TODO: return instrument info on transaction?
+        public Instrument Instrument { get; set; }
     }
 
     public enum TransactionState
