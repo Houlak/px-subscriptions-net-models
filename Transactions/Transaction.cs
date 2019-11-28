55ac323254c2106769070c6808b61d88f1bc6b0e
Lucas <lucas.suburuch@gmail.com>
2019-11-28 11:55:30 -0300

    subscription included in transaction

--- Transactions/Transaction.cs
+++ Transactions/Transaction.cs
@@ -7,9 +7,7 @@ namespace Goova.Subscriptions.Models.Transactions
     public class Transaction
     {
         public int Id { get; set; }
-        public string SubscriptionName { get; set; }
-        public string SubscriptionDescription { get; set; }
-        public string SubscriptionToken { get; set; }
+        public Subscriptions.Subscription Subscription { get; set; }
         public decimal Amount { get; set; }
         public DateTime CreatedAt { get; set; }
         public TransactionState State { get; set; }
