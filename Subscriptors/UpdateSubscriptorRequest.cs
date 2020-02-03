dcf63c2fab328dc2666d28a6707c87ebdfec9c6d
Lucas <lucas.suburuch@gmail.com>
2020-02-03 11:52:10 -0300

    postalCOde requested

--- Subscriptors/UpdateSubscriptorRequest.cs
+++ Subscriptors/UpdateSubscriptorRequest.cs
@@ -4,6 +4,7 @@
     {
         public string Email { get; set; }
         public string Name { get; set; }
+        public string PostalCode { get; set; }
         public string FullName { get; set; }
     }
 }
