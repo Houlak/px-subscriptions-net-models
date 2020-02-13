9a4ef4acb470bfc4698b07798de8f2fcd0f8d980
Lucas <lucas.suburuch@gmail.com>
2020-02-13 13:59:59 -0300

    filter subscriptors by active inactive

--- Subscriptors/SubscriptorsQueryAttribute.cs
+++ Subscriptors/SubscriptorsQueryAttribute.cs
@@ -6,7 +6,14 @@ namespace Goova.Subscriptions.Models.Subscriptors
     {
         public int Page { get; set; } = 1;
         public int Limit { get; set; } = 5;
-        public bool ActiveOnly { get; set; }
+        public SubscriptorsQueryState SubscriptorsState { get; set; }
         public string SubscriptionTypes { get; set; }
+    }
+
+    public enum SubscriptorsQueryState
+    {
+        All,
+        Active,
+        Inactive
     }
 }
