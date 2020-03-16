2d1d901d8069631317d73975c9da3ef6a00be2ac
Lucas Suburu <lucas.suburuch@gmail.com>
2020-03-16 14:49:36 -0300

    added search by external id

--- Subscriptors/SubscriptorsQueryAttribute.cs
+++ Subscriptors/SubscriptorsQueryAttribute.cs
@@ -6,7 +6,7 @@ namespace Goova.Subscriptions.Models.Subscriptors
     {
         public int Page { get; set; } = 1;
         public int Limit { get; set; } = 5;
-        public string SubscriptorEmailOrName { get; set; }
+        public string SubscriptorEmailOrNameOrId { get; set; }
         public SubscriptorsQueryState SubscriptorsState { get; set; }
         public string SubscriptionTypes { get; set; }
     }
