bbe80692872e200f4fca666427df7ef8785e097b
nicolas.cobelli <nicolas.cobelli@fing.edu.uy>
2020-11-18 10:20:15 -0300

    added constructor to model

--- Subscriptions/Subscription.cs
+++ Subscriptions/Subscription.cs
@@ -14,5 +14,48 @@ namespace Goova.Subscriptions.Models.Subscriptions
         public TransactionState SubscriptionStatus { get; set; }
         public DateTime SubscriptionLastPaymentDate { get; set; }
         public DateTime ValidUntil { get; set; }
+        public Subscription(string Id,string Token,DateTime SubscriptionDate, DateTime LastPaymentDate,TransactionState SubscriptionStatus, int IdSubscriptionType,string NameSubscriptionType, string DescriptionSubscriptionType,Frequence Frequence, Currency Currency,decimal Cost, string TaxPercentage,DateTime ValidUntil, string LegalId)
+        {
+            this.Id = Id;
+            this.Token = Token;
+            this.SubscriptionDate = SubscriptionDate;
+            this.SubscriptionLastPaymentDate = LastPaymentDate;
+            this.SubscriptionStatus = SubscriptionStatus;
+            this.SubscriptionType = new Models.SubscriptionType.SubscriptionType
+            {
+                Id = IdSubscriptionType,
+                Name = NameSubscriptionType,
+                Description = DescriptionSubscriptionType,
+                Frequence = Frequence,
+                Currency = Currency,
+                SubscriptionBaseCost = Cost,
+                TaxPercentage = TaxPercentage
+            };
+           this.ValidUntil = ValidUntil;
+           this.LegalId = LegalId;
+        }
+        // create without tax percentage 
+        public Subscription(string Id, string Token, DateTime SubscriptionDate, DateTime LastPaymentDate, TransactionState SubscriptionStatus, int IdSubscriptionType, string NameSubscriptionType, string DescriptionSubscriptionType, Frequence Frequence, Currency Currency, decimal Cost, DateTime ValidUntil, string LegalId)
+        {
+            this.Id = Id;
+            this.Token = Token;
+            this.SubscriptionDate = SubscriptionDate;
+            this.SubscriptionLastPaymentDate = LastPaymentDate;
+            this.SubscriptionStatus = SubscriptionStatus;
+            this.SubscriptionType = new Models.SubscriptionType.SubscriptionType
+            {
+                Id = IdSubscriptionType,
+                Name = NameSubscriptionType,
+                Description = DescriptionSubscriptionType,
+                Frequence = Frequence,
+                Currency = Currency,
+                SubscriptionBaseCost = Cost,
+            };
+            this.ValidUntil = ValidUntil;
+            this.LegalId = LegalId;
+        }
+
+        //generic constructor
+        public Subscription() { }
     }
 }
