a98d1c51a32d053557cacc242233e3ceb102440a
Nicolas Viera <nicolas.vierascafati@gmail.com>
2021-03-25 10:21:26 -0300

    submoduleSDK

--- Clients/UpdateClientRequest.cs
+++ Clients/UpdateClientRequest.cs
@@ -3,7 +3,7 @@
     public class UpdateClientRequest
     {
         public string ClientName { get; set; }
-        //public string ClientSecret { get; set; }
+        public string ClientSecret { get; set; }
         public string RecurringPaymentUrl { get; set; }
         public string LegalName { get; set; }
         public bool ElectronicBillingEnabled { get; set; }
