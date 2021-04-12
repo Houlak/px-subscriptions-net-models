cbe5841e88494d322ea54d6e8fb20f1de5fb3d76
Nicolas Viera <nicolas.vierascafati@gmail.com>
2021-04-12 10:28:01 -0300

    changes models

---
+++ Subscriptions/SubscriptionCancellationNotif.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Goova.Subscriptions.Models.Subscriptions
+{
+    public class SubscriptionCancellationNotif
+    {
+        public string SubscriptionType { get; set; }
+        public string SubscriptionActulStatus {get; set;}
+        public string ClientName { get; set; }
+        public string CancellationDate { get; set; }
+    }
+}
