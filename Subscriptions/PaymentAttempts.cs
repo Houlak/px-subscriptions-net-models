2f3dd8e2c1aa3564b4fe2c7389d7837176ebb270
nicolas.cobelli <nicolas.cobelli@fing.edu.uy>
2021-05-21 15:59:09 -0300

    notify when max retires

---
+++ Subscriptions/PaymentAttempts.cs
@@ -0,0 +1,12 @@
+﻿using Goova.Subscriptions.Models.Transactions;
+using System;
+
+namespace Goova.Subscriptions.Models.Subscriptions
+{
+    public class PaymentAttempts
+    {
+        public int TimesAttempted { get; set; }
+        public string ExternalId { get; set; }
+        public int SubscriptionTypeId { get; set; }
+    }
+}
