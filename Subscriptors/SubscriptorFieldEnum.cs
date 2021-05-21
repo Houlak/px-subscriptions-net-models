8f29188cfd8dc6cf046438fd4afedfc67a92164e
Nicolas Viera <nicolas.vierascafati@gmail.com>
2021-05-21 09:49:49 -0300

    new enum

---
+++ Subscriptors/SubscriptorFieldEnum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Goova.Subscriptions.Models.Subscriptors
+{
+    public enum SubscriptorFieldEnum
+    {
+        EXTERNALID,
+        SUBSCRIPTORID,
+        EMAIL,
+        NAMES,
+        SURNAMES,
+        TELEPHONENO,
+        BIRTHDATE,
+        CITY,
+        GENDER,
+        SUBSCRIPTIONCREATEDOK
+    }
+}
