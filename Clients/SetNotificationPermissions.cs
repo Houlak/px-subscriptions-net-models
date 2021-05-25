59d0215d3a0fa4a99f58962549b58fae478ef631
Nicolas Viera <nicolas.vierascafati@gmail.com>
2021-05-25 13:04:23 -0300

    new permissions notifications

---
+++ Clients/SetNotificationPermissions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Goova.Subscriptions.Models.Clients
+{
+    public class SetNotificationPermissions
+    {
+        public int? Id { get; set; } = null;
+        public bool TransactionCancellation { get; set; }
+        public bool SubscriptionCancellation { get; set; }
+        public bool SubscriptorFields { get; set; }
+        public bool PaymentSuccess { get; set; }
+        public bool PaymentFailed { get; set; }
+    }
+}
