b9729239f138ebddef276776d0ab472f5ddc6b91
Diego Tissot <juanchoztissot@gmail.com>
2021-06-04 15:34:59 -0300

    Add PF models

---
+++ PaymentFacilitator/AddPFRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Goova.Subscriptions.Models.PaymentFacilitator
+{
+    public class AddPFRequest
+    {
+        public string Name { get; set; }
+        public Style Style { get; set; }
+        public string AdminUserId { get; set; }
+        public List<int> PFCommerceIds { get; set; }
+        public int PaymentFacilitatorPlexoId { get; set; }
+    }
+}
---
+++ PaymentFacilitator/Style.cs
@@ -0,0 +1,9 @@
+﻿namespace Goova.Subscriptions.Models.PaymentFacilitator
+{
+    public class Style
+    {
+        public string PrimaryColor { get; set; }
+        public string SecondaryColor { get; set; }
+
+    }
+}
