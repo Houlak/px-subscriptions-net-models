03d193487aae8683820c44eca2f6d602f5ff5fcb
Diego Tissot <juanchoztissot@gmail.com>
2021-06-04 16:40:39 -0300

    Add request for PUT of PF

---
+++ PaymentFacilitator/EditPFRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Goova.Subscriptions.Models.PaymentFacilitator
+{
+    public class EditPFRequest
+    {
+        public string Name { get; set; }
+        public Style Style { get; set; }
+        public string AdminUserId { get; set; }
+        public List<int> PFCommerceIds { get; set; }
+        public int PaymentFacilitatorPlexoId { get; set; }
+
+    }
+}
