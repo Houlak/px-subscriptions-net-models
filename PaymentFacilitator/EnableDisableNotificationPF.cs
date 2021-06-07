c0e275921306fe6a1c9fd0ab6b4d517cd279d7bf
Nicolas Viera <nicolas.vierascafati@gmail.com>
2021-06-07 16:40:45 -0300

    notification

---
+++ PaymentFacilitator/EnableDisableNotificationPF.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Goova.Subscriptions.Models.PaymentFacilitator
+{
+    public class EnableDisableNotificationPF
+    {
+        public bool TransactionCancellation { get; set; }
+        public bool SubscriptionCancellation { get; set; }
+        public bool SubscriptorFields { get; set; }
+        public bool PaymentSuccess { get; set; }
+        public bool PaymentFailed { get; set; }
+        public string CallbackUrl { get; set; }
+        public Goova.Subscriptions.Models.Clients.RetriesConfigurationEnum RetriesConfiguration { get; set; }
+        public bool OnlyNotifications { get; set; } = false;
+    }
+}
