b3a76ff65a085e2e83136ae5e6f585423dc9f68d
nicolas.cobelli <nicolas.cobelli@fing.edu.uy>
2021-06-07 15:20:53 -0300

    added callbackurl to pf

--- PaymentFacilitator/PaymentFacilitator.cs
+++ PaymentFacilitator/PaymentFacilitator.cs
@@ -7,6 +7,7 @@ namespace Goova.Subscriptions.Models.PaymentFacilitator
     public class PaymentFacilitator
     {
         public Guid Id { get; set; }
+        public string CallbackUrl { get; set; }
         public string Name { get; set; }
         public Style Style { get; set; }
         public ICollection<int> PaymentFacilitatorCommerceIds { get; set; }
