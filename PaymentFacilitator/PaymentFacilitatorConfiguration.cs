9c95bd2cac51a30a4f7040dbf7c49b965cb94eaf
nicolas.cobelli <nicolas.cobelli@fing.edu.uy>
2021-06-07 16:41:17 -0300

    added pf config

---
+++ PaymentFacilitator/PaymentFacilitatorConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Goova.Subscriptions.Models.PaymentFacilitator
+{
+    public class PaymentFacilitatorConfiguration
+    {
+        public PaymentFacilitator PaymentFacilitator { get; set; }
+        public bool Enabled { get; set; }
+        public bool CanCreateAdditionalIssuers { get; set; }
+        public PaymentFacilitatorConfiguration() : base()
+        {
+
+        }
+    }
+}
