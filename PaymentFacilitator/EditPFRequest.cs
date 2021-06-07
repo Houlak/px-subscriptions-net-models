bc8a6104492e6dfd130f2cff1271c4030236be62
Diego Tissot <juanchoztissot@gmail.com>
2021-06-07 10:07:43 -0300

    Fix Edit PFRequest

--- PaymentFacilitator/EditPFRequest.cs
+++ PaymentFacilitator/EditPFRequest.cs
@@ -8,7 +8,6 @@ namespace Goova.Subscriptions.Models.PaymentFacilitator
     {
         public string Name { get; set; }
         public Style Style { get; set; }
-        public string AdminUserId { get; set; }
         public List<int> PFCommerceIds { get; set; }
         public int PaymentFacilitatorPlexoId { get; set; }
 
