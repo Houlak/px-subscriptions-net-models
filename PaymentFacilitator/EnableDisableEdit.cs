6ec5a680bb9faa0de4ffeb4f60bea78f2840f5f0
Nicolas Viera <nicolas.vierascafati@gmail.com>
2021-06-07 11:30:34 -0300

    modelo

---
+++ PaymentFacilitator/EnableDisableEdit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Goova.Subscriptions.Models.PaymentFacilitator
+{
+    public class EnableDisableEdit
+    {
+        public string ClientId { get; set;}
+        public bool Enable { get; set; }
+    }
+}
