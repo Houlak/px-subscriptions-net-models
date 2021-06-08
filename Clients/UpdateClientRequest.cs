dc8f12c9a0b7656785a1c74ddbca2966ed4b96b7
nicolas.cobelli <nicolas.cobelli@fing.edu.uy>
2021-06-08 09:20:40 -0300

    remove client secret from update

--- Clients/UpdateClientRequest.cs
+++ Clients/UpdateClientRequest.cs
@@ -3,7 +3,6 @@
     public class UpdateClientRequest
     {
         public string ClientName { get; set; }
-        public string ClientSecret { get; set; }
         public string RecurringPaymentUrl { get; set; }
         public string LegalName { get; set; }
         public bool ElectronicBillingEnabled { get; set; }
