a19eaa7a4a79d5fa430e1c4f2ac4be19e88038f5
nicolas.cobelli <nicolas.cobelli@fing.edu.uy>
2021-06-09 12:41:20 -0300

    return admin in get pf admin

---
+++ PaymentFacilitator/PFAdmin.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Goova.Subscriptions.Models.Clients
+{
+    public class PFAdmin
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+    }
+}
