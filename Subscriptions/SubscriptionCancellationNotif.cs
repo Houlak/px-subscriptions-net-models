7a7d581b60c141de1262aa6d495c65ebb7695625
Diego Tissot <juanchoztissot@gmail.com>
2021-06-17 18:00:06 -0300

    Add NotifType to notifications

--- Subscriptions/SubscriptionCancellationNotif.cs
+++ Subscriptions/SubscriptionCancellationNotif.cs
@@ -1,14 +1,17 @@
-﻿using System;
+﻿using Goova.Subscriptions.Models.Notifications;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Goova.Subscriptions.Models.Subscriptions
 {
-    public class SubscriptionCancellationNotif
+    public class SubscriptionCancellationNotif : Notification
     {
         public string SubscriptionType { get; set; }
         public string SubscriptionActulStatus {get; set;}
         public string ClientName { get; set; }
         public string CancellationDate { get; set; }
+        public new NotificationTypeEnum NotificationType { get; } = NotificationTypeEnum.SubscriptionCancellation;
+
     }
 }
