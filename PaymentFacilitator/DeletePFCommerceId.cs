debe3d4052b06a1814d4e08169c4fd735b1d3be9
Nicolas Viera <nicolas.vierascafati@gmail.com>
2021-06-24 10:31:41 -0300

    delete payment facilitator

---
+++ PaymentFacilitator/DeletePFCommerceId.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Goova.Subscriptions.Models.PaymentFacilitator
+{
+    public class DeletePFCommerceId
+    {
+        public string PFId { get; set; }
+        public int PFCommerceId { get; set; }
+    }
+}
---
+++ PaymentFacilitator/PaymentFacilitatorCommerceId.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Goova.Subscriptions.Models.PaymentFacilitator
+{
+    public class PaymentFacilitatorCommerceId
+    {
+        public int Id { get; set; }
+        public int Value { get; set; }
+        public PaymentFacilitator PaymentFacilitator { get; set; }
+    }
+}
