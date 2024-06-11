b4746b82bcd6edcc19115c91d8485d48149a3e87
Nicolas Viera <n.viera@houlak.com>
2024-06-11 15:39:17 -0300

    change get subscription state response

--- Subscriptors/SubscriptionStateResponse.cs
+++ Subscriptors/SubscriptionStateResponse.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Goova.Subscriptions.Models.Transactions;
+using System;
 
 namespace Goova.Subscriptions.Models.Subscriptors
 {
     public class SubscriptionStateResponse
     {
-        public bool Active { get; set; }
+        public bool Valid { get; set; }
         public SubscriptionType.SubscriptionType SubscriptionType { get; set; }
         public DateTime? ActiveUntil { get; set; }
+        public TransactionState SubscriptionStatus { get; set; }
     }
 }
